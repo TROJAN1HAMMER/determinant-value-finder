5ab70a52b424c79dd205ab84956941ac90560121
TROJAN1HAMMER <106213578+TROJAN1HAMMER@users.noreply.github.com>
2022-07-07 19:00:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,97 +1,42 @@
-# INTRO
-
 print("Hello!")
 print("Welcome to determinant .py calculator")
 
-print("For which order determinant, do you want to find the determinant value")
+def main():
+    print("! a b c !")
+    print("! d e f !")
+    print("! g h i !")
 
-y1 = input("Is it 2*2?")
+    a = int(input("What is the value of a: "))
+    b = int(input("What is the value of b: "))
+    c = int(input("What is the value of c: "))
+    d = int(input("What is the value of d: "))
+    e = int(input("What is the value of e: "))
+    f = int(input("What is the value of f: "))
+    g = int(input("What is the value of g: "))
+    h = int(input("What is the value of h: "))
+    i = int(input("What is the value of i: "))
 
-y_1 = y1.lower()
+    a_1 = (e*i) - (h*f)
+    b_1 = (d*i) - (g*f)
+    c_1 = (d*h) - (g*e)
 
-if y_1 == "yes":
-    # for 2*2 matrix
+    a_2 = a * a_1
+    b_2 = b * b_1
+    c_2 = c * c_1
 
-    print("Since it is a 2 * 2 matrices...")
+    determinant = a_2 - b_2 + c_2
 
-    # For looping and determining the value of 2 * 2 matrices
+    print("Given matrices is: ")
 
-    def main():
+    print("!", a, b, c, "!")
+    print("!", d, e, f, "!")
+    print("!", g, h, i, "!")
 
-        print("!", "p", "q", "!")
-        print("!", "r", "s", "!")
+    repeat = input("Is there any correction?")
+    if repeat == "yes":
+        main()
+    else:
+        print("The value of the give determinant is: ", determinant)
+        print("Thank you for using determinant .py calculator.")
 
-        p = int(input("What is the value of p: "))
-        q = int(input("What is the value of q: "))
-        r = int(input("What is the value of r: "))
-        s = int(input("What is the value of s: "))
-
-        print("Given matrices is:")
-        print("!", p, q, "!")
-        print("!", r, s, "!")
-
-        repeat_1 = input("Is there any corrections?")
-        repeat = repeat_1.lower()
-        if repeat == "yes":
-            main()
-
-        else:
-            determinant_2 = p * s - q * r
-
-            print("The value of the given determinant is: ", determinant_2)
-            print("Thank you for using determinant .py calculator.")
-
-    main()
-
-# For 3 * 3 matrices
-
-elif y_1 == "no":
-
-    print("Then since it is a 3 * 3 matrices..")
-
-    # For looping and 3 * 3 matrices calculations
-
-    def main():
-        print("! a b c !")
-        print("! d e f !")
-        print("! g h i !")
-
-        a = int(input("What is the value of a: "))
-        b = int(input("What is the value of b: "))
-        c = int(input("What is the value of c: "))
-        d = int(input("What is the value of d: "))
-        e = int(input("What is the value of e: "))
-        f = int(input("What is the value of f: "))
-        g = int(input("What is the value of g: "))
-        h = int(input("What is the value of h: "))
-        i = int(input("What is the value of i: "))
-
-        a_1 = (e * i) - (h * f)
-        b_1 = (d * i) - (g * f)
-        c_1 = (d * h) - (g * e)
-
-        a_2 = a * a_1
-        b_2 = b * b_1
-        c_2 = c * c_1
-
-        determinant_3 = a_2 - b_2 + c_2
-
-        print("Given matrices is: ")
-
-        print("!", a, b, c, "!")
-        print("!", d, e, f, "!")
-        print("!", g, h, i, "!")
-
-        repeat_1 = input("Is there any correction?")
-        repeat = repeat_1.lower()
-
-        if repeat == "yes":
-            main()
-        else:
-            print("The value of the give determinant is: ", determinant_3)
-            print("Thank you for using determinant .py calculator.")
-
-    main()
-
-# finished
-# It is used to find determinant value of 2 * 2 matrices and 3 * 3 matrices....
+main()
