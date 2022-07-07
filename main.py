889d040721aa2d7328ca88a3049378d41bc31e56
TROJAN1HAMMER <106213578+TROJAN1HAMMER@users.noreply.github.com>
2022-07-07 19:04:53 +0530

    added squared matrices value finding.

--- main.py
+++ main.py
@@ -1,42 +1,105 @@
+# INTRO
+
 print("Hello!")
 print("Welcome to determinant .py calculator")
 
-def main():
-    print("! a b c !")
-    print("! d e f !")
-    print("! g h i !")
+print("For which order determinant, do you want to find the determinant value")
 
-    a = int(input("What is the value of a: "))
-    b = int(input("What is the value of b: "))
-    c = int(input("What is the value of c: "))
-    d = int(input("What is the value of d: "))
-    e = int(input("What is the value of e: "))
-    f = int(input("What is the value of f: "))
-    g = int(input("What is the value of g: "))
-    h = int(input("What is the value of h: "))
-    i = int(input("What is the value of i: "))
+y1 = input("Is it 2*2?")
 
-    a_1 = (e*i) - (h*f)
-    b_1 = (d*i) - (g*f)
-    c_1 = (d*h) - (g*e)
+y_1 = y1.lower()
 
-    a_2 = a * a_1
-    b_2 = b * b_1
-    c_2 = c * c_1
+if y_1 == "yes":
+    # for 2*2 matrix
 
-    determinant = a_2 - b_2 + c_2
+    print("Since it is a 2 * 2 matrices...")
 
-    print("Given matrices is: ")
+    # For looping and determining the value of 2 * 2 matrices
 
-    print("!", a, b, c, "!")
-    print("!", d, e, f, "!")
-    print("!", g, h, i, "!")
+    def main():
 
-    repeat = input("Is there any correction?")
-    if repeat == "yes":
-        main()
-    else:
-        print("The value of the give determinant is: ", determinant)
-        print("Thank you for using determinant .py calculator.")
+        print("!", "p", "q", "!")
+        print("!", "r", "s", "!")
 
-main()
+        p = int(input("What is the value of p: "))
+        q = int(input("What is the value of q: "))
+        r = int(input("What is the value of r: "))
+        s = int(input("What is the value of s: "))
+
+        print("Given matrices is:")
+        print("!", p, q, "!")
+        print("!", r, s, "!")
+
+        repeat_1 = input("Is there any corrections?")
+        repeat = repeat_1.lower()
+        if repeat == "yes":
+            main()
+
+        else:
+            determinant_2 = p * s - q * r
+
+            a = p * p + r * q
+            b = p * q + q * s
+            c = p * r + r * s
+            d = q * r + s * s
+
+            print("The value of the given determinant is: ", determinant_2)
+            print("determinant squared is:")
+            print("!", a, b, "!")
+            print("!", c, d, "!")
+            print("Thank you for using determinant .py calculator.")
+
+    main()
+
+# For 3 * 3 matrices
+
+elif y_1 == "no":
+
+    print("Then since it is a 3 * 3 matrices..")
+
+    # For looping and 3 * 3 matrices calculations
+
+    def main():
+        print("! a b c !")
+        print("! d e f !")
+        print("! g h i !")
+
+        a = int(input("What is the value of a: "))
+        b = int(input("What is the value of b: "))
+        c = int(input("What is the value of c: "))
+        d = int(input("What is the value of d: "))
+        e = int(input("What is the value of e: "))
+        f = int(input("What is the value of f: "))
+        g = int(input("What is the value of g: "))
+        h = int(input("What is the value of h: "))
+        i = int(input("What is the value of i: "))
+
+        a_1 = (e * i) - (h * f)
+        b_1 = (d * i) - (g * f)
+        c_1 = (d * h) - (g * e)
+
+        a_2 = a * a_1
+        b_2 = b * b_1
+        c_2 = c * c_1
+
+        determinant_3 = a_2 - b_2 + c_2
+
+        print("Given matrices is: ")
+
+        print("!", a, b, c, "!")
+        print("!", d, e, f, "!")
+        print("!", g, h, i, "!")
+
+        repeat_1 = input("Is there any correction?")
+        repeat = repeat_1.lower()
+
+        if repeat == "yes":
+            main()
+        else:
+            print("The value of the give determinant is: ", determinant_3)
+            print("Thank you for using determinant .py calculator.")
+
+    main()
+
+# finished
+# It is used to find determinant value of 2 * 2 matrices and 3 * 3 matrices....
